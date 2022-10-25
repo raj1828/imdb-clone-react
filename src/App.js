8493968e351f87282d68ddb73baa141c228bf6ce
Raj Mhatre <rajjimh18phpcomp@student.mes.ac.in>
2022-10-26 00:29:46 +0530

    Final commite 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './Components/Header/Header';
 import { Home } from './Pages/Home/Home';
 import { Card } from './Components/Card/Card';
 import MovieList from './Components/movieList/movieList';
+import Movie from './Pages/movieDetail/movie';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>Movie Deatil Page</h1>}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
